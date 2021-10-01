6e539372eac1b066903ab72f1084cb5c8a717f10
lilei <1434269333@qq.com>
2021-10-01 16:11:42 +0800

    添加注释

--- mybatis-demo/src/main/java/ex01/MybatisDemo.java
+++ mybatis-demo/src/main/java/ex01/MybatisDemo.java
@@ -20,14 +20,23 @@ import javax.sql.DataSource;
 public class MybatisDemo {
 
     public static void main(String[] args) {
+        // 准备工作：创建数据源 DataSource
         DataSource dataSource = getDataSource();
+        // 准备工作：实例化一个 TransactionFactory
         TransactionFactory transactionFactory = new JdbcTransactionFactory();
+        // 准备工作：通过持有数据源 DataSource 引用和 TransactionFactory 引用，创建环境配置 Environment 实例
         Environment environment = new Environment("development", transactionFactory, dataSource);
+        // 准备工作：持有环境配置 Environment 引用，创建基本的 Configuration 实例
         Configuration configuration = new Configuration(environment);
+        // 第一步：把 BlogMapper interface 添加到 mapperRegistry 注册仓库中
         configuration.addMapper(BlogMapper.class);
+        // 第二步：通过 SqlSessionFactoryBuilder.build 构建 SqlSessionFactory 工厂
         SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(configuration);
+        // 第三步：通过 SqlSessionFactory 工厂创建一个 SqlSession
         try (SqlSession session = sqlSessionFactory.openSession()) {
+            // 第四步：从 session 中拿到 BlogMapper 的代理对象
             BlogMapper mapper = session.getMapper(BlogMapper.class);
+            // 第五步：代理对象通过反射进行动态代理，最后得到返回结果
             Blog blog = mapper.selectBlog(1);
             System.out.println(blog);
         } catch (Exception e) {
